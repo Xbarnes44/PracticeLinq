3b6a32273e8fd7c783d1d8346cd82b01098f42a9
Xavier Barnes <xdbcoder44@gmail.com>
2022-09-28 00:06:32 -0400

    Add project files.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqPractice
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            var videogames = new List<string> {"Call of duty", "Halo", "Zelda", "Apex legends" } ;
+
+            var gameLength = videogames.OrderBy(videogames => videogames.Length);
+
+            foreach (var x in gameLength)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
